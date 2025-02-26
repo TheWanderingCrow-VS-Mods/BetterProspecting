621bd499a5ee2eeddc76e79f4a77b1d4dd8675e7
TheWanderingCrow <contact@wanderingcrow.net>
2025-02-26 11:48:10 -0500

    flake build system

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "VintageStory build flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {
+      inherit inputs;
+    } {
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "BetterProspecting";
+          version = builtins.substring 0 8 self.rev or "dirty";
+
+          src = pkgs.lib.cleanSource ./.;
+
+          nativeBuildInputs = with pkgs; [
+            dotnet-sdk_7
+            vintagestory
+          ];
+
+          buildPhase = ''
+            export VINTAGE_STORY="${pkgs.vintagestory}/share"
+            dotnet run --project BetterProspecting/CakeBuild/CakeBuild.csproj
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r BetterProspecting/Releases/* $out/
+          '';
+        };
+      };
+    };
+}
